87913c04440f8404dc982d8ba7e714277f263770
Van Labajo <van.labajo@serialmultivision.com>
2021-06-11 19:14:06 +0800

    Added routing to product-form

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,15 +5,19 @@ import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     ProductsComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductFormComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     NgbPaginationModule,
     ProductsRoutingModule
   ]
