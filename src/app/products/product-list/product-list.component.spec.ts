50d66e1693d21c1c54904272fbb8ebbb3d6f003a
Van Labajo <van.labajo@serialmultivision.com>
2021-06-11 19:13:02 +0800

    Updated product-list to have pagination, search box and navigation to product-form

--- src/app/products/product-list/product-list.component.spec.ts
+++ src/app/products/product-list/product-list.component.spec.ts
@@ -1,7 +1,10 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { HttpErrorHandler } from '../../http-error-handler/http-error-handler.service';
 import { ProductListComponent } from './product-list.component';
+
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
@@ -10,7 +13,8 @@ describe('ProductListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProductListComponent],
-      imports: [NgbPaginationModule]
+      imports: [NgbPaginationModule, HttpClientTestingModule, RouterTestingModule],
+      providers: [HttpErrorHandler]
     })
     .compileComponents();
   });
@@ -23,5 +27,39 @@ describe('ProductListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should hide pagination when pagedResult.collectionSize < pagedQuery.pageInfo.pageSize', () => {
+    component.pagedResult.collectionSize = 1;
+    component.pagedQuery.pageInfo.pageSize = 30;
+    fixture.detectChanges();
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const pagination = nativeElement.querySelector('ngb-pagination')!;
+    expect(pagination).toBeNull();
+  });
+
+  it('should show pagination when pagedResult.collectionSize > pagedQuery.pageInfo.pageSize', () => {    
+    component.pagedResult.collectionSize = 31;
+    component.pagedQuery.pageInfo.pageSize = 30;
+    fixture.detectChanges();
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const pagination = nativeElement.querySelector('ngb-pagination')!;    
+    expect(pagination).not.toBeNull();
+  });
+
+  it('should display no records found when product data is empty', () => {
+    component.pagedResult.data = [];
+    fixture.detectChanges();
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const td = nativeElement.querySelector('td')!;
+    expect(td.textContent).toEqual('No records found');
+  });
+
+  it('should not display no records found when product data exists', () => {
+    component.pagedResult.data = [{ id: 1, name: 'coffee', upc: '10001', price: 0, cost: 0, quantity: 10 }];
+    fixture.detectChanges();
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const td = nativeElement.querySelector('td')!;
+    expect(td.textContent).not.toEqual('No records found');
   });
 });
