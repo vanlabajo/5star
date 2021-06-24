6fdd5ae104753a4335b2591cd9ded7bcac133f4f
Van Labajo <van.labajo@serialmultivision.com>
2021-06-24 13:13:19 +0800

    Consumed checkout backend api

--- src/app/pos/cart.service.spec.ts
+++ src/app/pos/cart.service.spec.ts
@@ -1,14 +1,27 @@
+import { HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { HttpErrorHandler } from '../http-error-handler/http-error-handler.service';
+import { ServiceResult } from '../models/service-result.interface';
+import { Product } from '../products/product.interface';
 import { CartItem } from './cart-item.interface';
 import { CartService } from './cart.service';
 
 
 describe('CartService', () => {
+  let httpTestingController: HttpTestingController;
   let service: CartService;
   let expectedItems: CartItem[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      // Import the HttpClient mocking services
+      imports: [HttpClientTestingModule],
+      // Provide the service-under-test and its dependencies
+      providers: [CartService, HttpErrorHandler]
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(CartService);
 
     expectedItems = [
@@ -56,6 +69,39 @@ describe('CartService', () => {
     expect(service.items.length).toEqual(1, 'should return 1 item');
     expect(service.items[0].product.name).toEqual('B', 'should return product "B"');
     expect(service.items[0].quantity).toEqual(2, 'should return 2 quantity');
+  });
+
+  it('should reset or void cart', () => {
+
+    service.addItem(expectedItems[0].product);
+    service.addItem(expectedItems[1].product);
+    service.addItem(expectedItems[1].product);
+
+    service.resetCart();
+
+    expect(service.items.length).toEqual(0, 'should return empty cart');
+  });
+
+  it('should checkout the cart and return a ServiceResult', () => {
+    const product: Product = { id: 0, name: 'A', upc: '1001', cost: 1, price: 1, quantity: 10 };
+
+    service.addItem(product);
+
+    service.checkout().subscribe(
+      serviceResult => expect(serviceResult.success).toEqual(true, 'should return successful'),
+      fail
+    );
+
+    // CartService should have made one request to POST product
+    const req = httpTestingController.expectOne(service.checkoutUrl);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(service.items.map(item => ({ productId: item.product.id, quantity: item.quantity })));
+
+    // Expect server to return the ServiceResult after POST
+    const expectedResponse = new HttpResponse({
+      status: 200, statusText: 'OK', body: { success: true } as ServiceResult
+    });
+    req.event(expectedResponse);
   });
 
 });
--- src/app/pos/cart.service.ts
+++ src/app/pos/cart.service.ts
@@ -1,4 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { HandleError, HttpErrorHandler } from '../http-error-handler/http-error-handler.service';
+import { ServiceResult } from '../models/service-result.interface';
 import { Product } from '../products/product.interface';
 import { CartItem } from './cart-item.interface';
 
@@ -6,10 +12,17 @@ import { CartItem } from './cart-item.interface';
   providedIn: 'root'
 })
 export class CartService {
+  readonly checkoutUrl: string;
   private _items: CartItem[];
+  private handleError: HandleError;
 
-  constructor() {
+  constructor(
+    private http: HttpClient,
+    httpErrorHandler: HttpErrorHandler
+  ) {
+    this.checkoutUrl = `${environment.baseUrl}/api/checkout`;
     this._items = [];
+    this.handleError = httpErrorHandler.createHandleError('CartService');
   }
 
   get items(): CartItem[] {
@@ -38,6 +51,17 @@ export class CartService {
 
   removeItem(product: Product): void {
     this._items = this._items.filter(cartItem => cartItem.product.id !== product.id);
+  }
+
+  resetCart(): void {
+    this._items = [];
+  }
+
+  checkout(): Observable<ServiceResult> {
+    return this.http.post<ServiceResult>(this.checkoutUrl, this.items.map(item => ({ productId: item.product.id, quantity: item.quantity })))
+      .pipe(
+        catchError(this.handleError('checkout', { success: false, validationErrors: undefined } as ServiceResult))
+      );
   }
 
 }
