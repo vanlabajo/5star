521bd2f2805db96af0aae1d8aae580289b0ee011
Van Labajo <van.labajo@serialmultivision.com>
2021-07-05 15:20:05 +0800

    Updated invoice-detail.component.spec.ts

--- src/app/invoices/invoice-detail/invoice-detail.component.spec.ts
+++ src/app/invoices/invoice-detail/invoice-detail.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpErrorHandler } from '../../http-error-handler/http-error-handler.service';
+import { InvoiceListComponent } from '../invoice-list/invoice-list.component';
 import { InvoiceService } from '../invoice.service';
 
 import { InvoiceDetailComponent } from './invoice-detail.component';
@@ -13,7 +14,11 @@ describe('InvoiceDetailComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [InvoiceDetailComponent],
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [
+        RouterTestingModule.withRoutes(
+          [{ path: 'invoices', component: InvoiceListComponent }]
+        ),
+        HttpClientTestingModule],
       providers: [InvoiceService, HttpErrorHandler]
     })
     .compileComponents();
