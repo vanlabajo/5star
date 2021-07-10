7843a6993ab0c03e650ca414795385f2f97a0069
Van Labajo <van.labajo@serialmultivision.com>
2021-07-10 12:55:52 +0800

    Handled landscape orientation issue

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -9,7 +9,8 @@ import { SpinnerService } from './spinner.service';
     </div>
   `,
   styles: [
-    '.fixed-above-nav-bar { position: fixed; z-index: 1030; right: 0; bottom: 9vh; opacity: 0; animation: .75s linear infinite spinner-grow; }'
+    '.fixed-above-nav-bar { position: fixed; z-index: 1030; right: 0; bottom: 9vh; opacity: 0; animation: .75s linear infinite spinner-grow; }',
+    '@media (orientation: landscape) { .fixed-above-nav-bar { bottom: 19vh } }'
   ]
 })
 export class SpinnerComponent {
